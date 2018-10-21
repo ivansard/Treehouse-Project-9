e8babee32bf71d0d1c24c08d561b3acc3ce11148
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-10-21 18:22:14 +0200

    API key imported into App.js

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Gallery from './components/Gallery';
+import apiKey from './config/config';
 
 class App extends Component {
+
+  state = { 
+    apiKey: apiKey
+  }
+
+  componentDidMount(){
+    console.log(this.state.apiKey);
+  }
+
+
   render() {
     return (
       <div className="App">
